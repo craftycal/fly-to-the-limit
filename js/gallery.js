e831677ed5ca2572e8363cd4ff95889026c11864
craftycal <callum.powell.1@gmail.com>
2016-04-20 09:44:15 +1200

    skelington slider

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,63 @@
 // get all the links of the images
-var.allImages = document.querySelectorAll("#images")
+var allImages = document.querySelectorAll("#images img")
 
 var imageSoursces = [];
 
-for (var i = 0; i < imageSoursces.length; i++) {
-	
-};
+for (var i = 0; i < allImages.length; i++) {
+	imageSoursces.push( allImages[i].src);
+}
+
+// delete all images on page
+var galleryContainer = document.querySelector("#gallery");
+
+galleryContainer.innerHTML = "";
+
+// create container slider
+var slider = document.createElement("div");
+
+// attach id to slider
+slider.id = "slider";
+
+// add slider the browser 
+galleryContainer.appendChild( slider);
+
+// create buttons
+var next = document.createElement("button");
+var prev = document.createElement("button");
+
+// add button id's
+next.id = "next";
+prev.id = "prev";
+
+next.innerHTML = "next";
+prev.innerHTML = "prev"; 
+
+// add buttons to slider
+slider.appendChild(next);
+slider.appendChild(prev);
+
+// show the firest image
+slider.style.backgroundImage = "url("+ imageSoursces[0] +")";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
